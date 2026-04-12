bbdda75db0e600d2b83d1d922b027e1581555011
karl <moreno_karla@live.com>
2026-04-12 15:15:00 -0500

    add oat css and js to repo

--- src/Views/layouts/main.php
+++ src/Views/layouts/main.php
@@ -3,9 +3,9 @@
 	<head>
 		<meta charset="UTF-8">
 		<title><?= htmlspecialchars($title ?? 'App') ?></title>
-		<link rel="stylesheet" href="https://unpkg.com/@knadh/oat/oat.min.css">
-		<script src="https://unpkg.com/@knadh/oat/oat.min.js" defer></script>
-		<link rel="stylesheet" href="/css/app.css">
+		<link rel="stylesheet" href="/css/oat.min.css"/>
+		<script src="/scripts/oat.min.js" defer></script>
+		<link rel="stylesheet" href="/css/app.css"/>
 	</head>
 	<body>
 		<?php require __DIR__ . '/../partials/nav.php'; ?>
