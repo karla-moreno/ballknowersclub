b5eae5d08716cdfeedab0831ad68e6334a09c721
karl <moreno_karla@live.com>
2026-04-17 20:34:43 -0500

    nav links

--- src/Views/partials/nav.php
+++ src/Views/partials/nav.php
@@ -6,10 +6,14 @@
 ?>
 <nav class="container" style="padding-top: var(--space-4);">
 	<?php if (Auth::check()): ?>
-		<span>
+		<span style="display: block;">
 			<?= Greeting::forSession() ?>, <?= htmlspecialchars(Auth::user()['name']); ?>
 		</span>
-		<a href="/logout.php">Logout</a>
+		<div class="hstack" style="font-size: 13px;">
+			<a href="/" style="text-transform: uppercase;">Home</a>
+			<a href="/draft/index.php" style="text-transform: uppercase;">Draft</a>
+			<a href="/logout.php" style="text-transform: uppercase;">Logout</a>
+		</div>
 	<?php else: ?>
 		<a href="/login.php">Login</a>
 	<?php endif; ?>
