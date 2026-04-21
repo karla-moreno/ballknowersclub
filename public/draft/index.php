193dfee3026db23dec5b214233468c86477b1ae4
karl <moreno_karla@live.com>
2026-04-21 00:36:04 -0500

    simplify draft pick UI, polling live update commit,  disable last picked team via polling

--- public/draft/index.php
+++ public/draft/index.php
@@ -1,93 +1,90 @@
 <?php
-	declare(strict_types=1);
+  declare(strict_types=1);
 
-	require_once __DIR__ . '/../../vendor/autoload.php';
+  require_once __DIR__ . '/../../vendor/autoload.php';
 
-	use App\Auth\Auth;
-	use App\Database\Database;
-	use App\Services\DraftService;
-	use App\Services\NbaApiService;
-	use App\Enums\Season;
+  use App\Auth\Auth;
+  use App\Database\Database;
+  use App\Services\DraftService;
+  use App\Services\NbaApiService;
+  use App\Enums\Season;
 
-	DraftService::createDraftTable();
+  DraftService::createDraftTable();
 
-	$db = Database::connection();
-	$teams = NbaApiService::allTeams();
-	$user = Auth::user();
+  $db = Database::connection();
+  $teams = NbaApiService::allTeams();
+  $user = Auth::user();
 
-	$title = 'Draft';
-	ob_start();
+  $title = 'Draft';
+  ob_start();
 
-	$draft_order = ['okc_glazer', 'Anonymous', 'deadmau5', 'zombiekilla', 'woosah'];
-	$draft_season = Season::S25_26->value;
+  $draft_order = ['okc_glazer', 'Anonymous', 'deadmau5', 'zombiekilla', 'woosah'];
+  $draft_season = Season::S25_26->value;
 
-	$picked_teams = $db->prepare("SELECT team_id FROM draft_picks WHERE season = ?");;
-	$picked_teams->execute([$draft_season]);
-	$picked_teams = $picked_teams->fetchAll(PDO::FETCH_COLUMN) ?? null;
-	// https://www.php.net/manual/en/pdostatement.fetchall.php
+  $picked_teams = $db->prepare("SELECT team_id FROM draft_picks WHERE season = ?");;
+  $picked_teams->execute([$draft_season]);
+  $picked_teams = $picked_teams->fetchAll(PDO::FETCH_COLUMN) ?? null;
+  // https://www.php.net/manual/en/pdostatement.fetchall.php
 
-	$num_picks = 5;
+  //	$num_picks = 5;
 ?>
-<?php include __DIR__ . '/../../src/Views/partials/picks.php'; ?>
-<?php if (Auth::check()): ?>
 	<div class="row">
-		<?php
-			for ($x = 0; $x <= $num_picks; $x++) {
-				$pick = $x + 1;
-				$disabled = $x > 0 ? 'disabled' : '';
-				?>
-				<article class="card col-4">
-					<header>
-						<h3>Team <?= $pick; ?></h3>
-					</header>
-					<div data-field>
-						<label>Team</label>
-						<select
-							aria-label="Select an option"
-							id="team-pick-<?= $pick; ?>"
-							<?= $disabled; ?>
-						>
-							<option value="">Select an option</option>
-							<?php foreach ($teams as $team) { ?>}
-								<option
-									value="<?= $team['team_id']; ?>"
-									<?= in_array($team['team_id'], $picked_teams) ? 'disabled'
-										: '' ?>>
-									<?= $team['name']; ?>
-								</option>
-							<?php } ?>
-						</select>
-					</div>
-					<fieldset
-						class="hstack"
-						<?= $disabled; ?>
+    <?php if (Auth::check()): ?>
+
+			<article class="card col-5">
+				<header>
+					<h3>Pick</h3>
+				</header>
+				<div data-field>
+					<label>Team</label>
+					<select
+						aria-label="Select an option"
+						id="pick-team"
 					>
-						<legend>Selection</legend>
-						<label>
-							<input type="radio"
-										 name="pick-<?= $pick; ?>"
-										 value="wins">
-							<span class="badge success">WINS</span>
-						</label>
-						<label>
-							<input type="radio"
-										 name="pick-<?= $pick; ?>"
-										 value="losses">
-							<span class="badge danger">LOSSES</span>
-						</label>
-					</fieldset>
-					<footer class="hstack">
-						<button commandfor="commit-dialog"
-										command="show-commit-confirmation"
-										id="commit-<?= $pick; ?>"
-										data-pick="<?= $pick ?>"
-							<?= $disabled; ?>
-						>
-							Commit
-						</button>
-					</footer>
-				</article>
-			<?php } ?>
+						<option value="">Select an option</option>
+            <?php foreach ($teams as $team) { ?>
+							<option
+								value="<?= $team['team_id']; ?>"
+                <?= in_array($team['team_id'], $picked_teams) ? 'disabled'
+                  : '' ?>>
+                <?= $team['name']; ?>
+							</option>
+            <?php } ?>
+					</select>
+				</div>
+				<fieldset
+					class="hstack"
+				>
+					<legend>Selection</legend>
+					<label>
+						<input type="radio"
+									 name="selection"
+									 id="selection-wins"
+									 value="wins">
+						<span class="badge success">WINS</span>
+					</label>
+					<label>
+						<input type="radio"
+									 name="selection"
+									 id="selection-losses"
+									 value="losses">
+						<span class="badge danger">LOSSES</span>
+					</label>
+				</fieldset>
+				<footer class="hstack">
+					<button commandfor="commit-dialog"
+									command="show-commit-confirmation"
+									id="commit"
+									data-pick=""
+					>
+						Commit
+					</button>
+				</footer>
+			</article>
+    <?php endif; ?>
+		<div class="col-7">
+      <?php include __DIR__ . '/../../src/Views/partials/picks.php'; ?>
+		</div>
 	</div>
 	<dialog id="commit-dialog" closedby="any">
 		<form method="dialog">
@@ -96,7 +93,7 @@
 				<p id="dialog-subheading"></p>
 			</header>
 			<div class="hstack items-center" style="justify-content: center;
-			transform: scale(1.2);">
+      transform: scale(1.2);">
 				<img id="dialog-logo" height="50"/>
 				<p id="dialog-team" style="font-size: 18px;"></p>
 				<span id="dialog-pick" class="badge"></span>
@@ -119,9 +116,10 @@
 		document.querySelectorAll('[data-pick]').forEach(button => {
 			button.addEventListener('click', () => {
 				const pick = button.dataset.pick;
-				const team = document.getElementById(`team-pick-${pick}`)?.selectedOptions[0]?.text;
-				const teamId = document.getElementById(`team-pick-${pick}`)?.selectedOptions[0]?.value;
-				const selection = document.querySelector(`input[name="pick-${pick}"]:checked`)?.value;
+				const team = document.getElementById(`pick-team`)?.selectedOptions[0]?.text;
+				const teamId = document.getElementById(`pick-team`)?.selectedOptions[0]?.value;
+				const selection = document.querySelector
+				(`input[name="selection"]:checked`)?.value;
 
 				dialogSubhead.textContent = `Team ${pick}`;
 				dialogTeam.textContent = team;
@@ -139,13 +137,34 @@
 	</script>
 	<script id="commit-logic">
 		let currentPick = {};
+		let lastQueuePickId = <?= $latestPickId ?? 'null' ?>;
+
+		setInterval(async () => {
+			try {
+				const res = await fetch('/draft/latest-pick.php');
+				const pick = await res.json();
+
+				if (pick.id && pick.id !== lastQueuePickId) {
+					lastQueuePickId = pick.id;
+					disablePickedTeam(pick.team_id.toString());
+				}
+			} catch (err) {
+				console.error('Polling error:', err);
+			}
+		}, 2000);
+
+		function disablePickedTeam(teamId) {
+			const option = document.querySelector(`#pick-team
+      option[value="${teamId}"]`);
+			if (option) option.disabled = true;
+		}
 
 		document.querySelectorAll('[data-pick]').forEach(button => {
 			button.addEventListener('click', () => {
 				const pick = button.dataset.pick;
-				const teamSelect = document.getElementById(`team-pick-${pick}`);
+				const teamSelect = document.getElementById(`pick-team`);
 				const teamName = teamSelect?.selectedOptions[0]?.text;
-				const selection = document.querySelector(`input[name="pick-${pick}"]:checked`)?.value;
+				const selection = document.querySelector(`input[name="selection"]:checked`)?.value;
 
 				currentPick = {
 					number: pick,
@@ -181,7 +200,7 @@
 							placement: 'top-center'
 						}
 					);
-					// TODO: enable next pick card, disable last pick
+					// TODO: enable next pick card
 				} else {
 					showDialogError(data.message ?? 'Something went wrong.');
 				}
@@ -205,7 +224,6 @@
 			err.textContent = message;
 		}
 	</script>
-<?php endif; ?>
 <?php
-	$content = ob_get_clean();
-	require __DIR__ . '/../../src/Views/layouts/main.php';
+  $content = ob_get_clean();
+  require __DIR__ . '/../../src/Views/layouts/main.php';
