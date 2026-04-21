ba80a8aca898d59ce569faf193aa4499f84fad8b
karl <moreno_karla@live.com>
2026-04-20 23:50:10 -0500

    wrap latest pick endpoint logic in try-catch for error handling

--- public/draft/latest-pick.php
+++ public/draft/latest-pick.php
@@ -1,13 +1,29 @@
 <?php
 	require_once __DIR__ . '/../../src/Database/Database.php';
+	require_once __DIR__ . '/../../src/Services/DraftService.php';
 
 	use App\Database\Database;
+	use App\Services\DraftService;
 
-	$db = Database::connection();
-	//	$pick = $db->query("SELECT * FROM draft_picks ORDER BY id DESC LIMIT 1")->fetch();
-	$pick = $db->query("SELECT pick.*, team.name as team_name
-    FROM draft_picks pick
-    JOIN teams team ON pick.team_id = team.team_id
-    ORDER BY pick.id DESC LIMIT 1")->fetch();
+	$draft_service = new DraftService();
+	try {
+		$draft_service::createDraftTable();
+	} catch (PDOException $e) {
+		error_log('Table already exists.');
+	}
 
-	echo json_encode($pick ?: []);
+	try {
+		//	$latest_pick = $draft_service::getLatestPick();
+
+		$db = Database::connection();
+		//	$pick = $db->query("SELECT * FROM draft_picks ORDER BY id DESC LIMIT 1")->fetch();
+		$pick = $db->query("
+			SELECT pick.*, team.name as team_name
+	    FROM draft_picks pick
+	    JOIN teams team ON pick.team_id = team.team_id
+	    ORDER BY pick.id DESC LIMIT 1
+    ")->fetch();
+		echo json_encode($pick ?: []);
+	} catch (Exception $e) {
+		error_log($e->getMessage());
+	}
