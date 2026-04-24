b88d181becac42ba545fa9b8af0184a2bab64f60
karl <moreno_karla@live.com>
2026-04-23 23:23:44 -0500

    nav formatting/register link

--- src/Views/partials/nav.php
+++ src/Views/partials/nav.php
@@ -1,20 +1,23 @@
 <?php
 
-	use App\Auth\Auth;
-	use App\Helpers\Greeting;
+  use App\Auth\Auth;
+  use App\Helpers\Greeting;
 
 ?>
 <nav class="container" style="padding-top: var(--space-4);">
-	<?php if (Auth::check()): ?>
-		<span style="display: block;">
+  <?php if (Auth::check()): ?>
+    <span style="display: block;">
 			<?= Greeting::forSession() ?>, <?= htmlspecialchars(Auth::user()['name']); ?>
 		</span>
-		<div class="hstack" style="font-size: 13px;">
-			<a href="/" style="text-transform: uppercase;">Home</a>
-			<a href="/draft/index.php" style="text-transform: uppercase;">Draft</a>
-			<a href="/logout.php" style="text-transform: uppercase;">Logout</a>
-		</div>
-	<?php else: ?>
-		<a href="/login.php">Login</a>
-	<?php endif; ?>
+    <div class="hstack" style="font-size: 13px; text-transform: uppercase;">
+      <a href="/">Home</a>
+      <a href="/draft/index.php">Draft</a>
+      <a href="/logout.php">Logout</a>
+    </div>
+  <?php else: ?>
+    <div class="hstack" style="font-size: 13px; text-transform: uppercase;">
+      <a href="/login.php">Login</a>
+      <a href="/register.php">Register</a>
+    </div>
+  <?php endif; ?>
 </nav>
