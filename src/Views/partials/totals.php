de54ddeb1be916e9c575febc7d774a8d6c2ec6d8
karl <moreno_karla@live.com>
2026-04-23 23:58:47 -0500

    reformat totals

--- src/Views/partials/totals.php
+++ src/Views/partials/totals.php
@@ -1,18 +1,18 @@
 <?php
-	declare(strict_types=1);
-	require_once __DIR__ . '/../../../vendor/autoload.php';
+  declare(strict_types=1);
+  require_once __DIR__ . '/../../../vendor/autoload.php';
 
-	use App\Auth\Auth;
-	use App\Database\Database;
-	use function App\Helpers\GetLogo;
+  use App\Auth\Auth;
+  use App\Database\Database;
+  use function App\Helpers\GetLogo;
 
-	$db = Database::connection();
-	//	$picks = $db->query("
-	//    SELECT pick.*, team.name as team_name
-	//    FROM draft_picks pick
-	//    JOIN teams team ON pick.team_id = team.team_id
-	//	")->fetchAll();
-	$test = $db->query("
+  $db = Database::connection();
+  //	$picks = $db->query("
+  //    SELECT pick.*, team.name as team_name
+  //    FROM draft_picks pick
+  //    JOIN teams team ON pick.team_id = team.team_id
+  //	")->fetchAll();
+  $test = $db->query("
 		SELECT pick.*, team.name as team_name, team_record.wins, team_record.losses,
 			CASE 
 				WHEN pick.skin_select = 'wins' THEN team_record.wins
@@ -22,82 +22,82 @@
 		JOIN teams team ON pick.team_id = team.team_id
 		JOIN team_records team_record ON pick.team_id = team_record.team_id
 	")->fetchAll();
-	$grouped = [];
-	foreach ($test as $pick) {
-		$grouped[$pick['username']][] = $pick;
-	}
-	ksort($grouped);
+  $grouped = [];
+  foreach ($test as $pick) {
+    $grouped[$pick['username']][] = $pick;
+  }
+  ksort($grouped);
 ?>
 
 <section class="section">
-	<div class="row">
-		<div class="col-12"><h2 style="margin-bottom: 0;">Totals</h2></div>
-		<?php foreach ($grouped as $username => $picks):
-			$total = array_sum(array_column($picks, 'skins'));
-			?>
-			<article class="card col-6">
-				<header>
-					<h3>
-						<?= $username; ?>
+  <div class="row">
+    <div class="col-12">
+      <h2 style="margin-bottom: 0;">Totals</h2>
+    </div>
+    <?php foreach ($grouped as $username => $picks):
+      $total = array_sum(array_column($picks, 'skins'));
+      ?>
+      <article class="card col-6">
+        <header>
+          <h3>
+            <?= $username; ?>
 
-						<?= Auth::check() && $username === htmlspecialchars
-						(Auth::user()
-						['name']) ?
-							'<span style="opacity: 0.5;">(you)</span>' :
-							'';
-						?>
-					</h3>
-				</header>
+            <?= Auth::check() && $username === htmlspecialchars(Auth::user()['username']) ?
+              '<span style="opacity: 0.5;">(you)</span>' :
+              '';
+            ?>
+          </h3>
+        </header>
 
-				<div class="table">
-					<table>
-						<thead>
-						<tr>
-							<th>Team</th>
-							<th>Selected</th>
-							<th>Wins</th>
-							<th>Losses</th>
-							<th>Skins</th>
-						</tr>
-						</thead>
-						<tbody>
-						<?php foreach ($picks as $pick): ?>
-							<tr>
-								<td>
-									<div class="flex items-center">
-										<img src="<?= getLogo($pick['team_id']); ?>"
-												 alt="<?= $pick['team_name']; ?> logo"
-												 height="35"/>
-										<span><?= $pick['team_name']; ?></span>
-									</div>
-								</td>
-								<td>
+        <div class="table">
+          <table>
+            <thead>
+            <tr>
+              <th>Team</th>
+              <th>Selected</th>
+              <th>Wins</th>
+              <th>Losses</th>
+              <th>Skins</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($picks as $pick): ?>
+              <tr>
+                <td>
+                  <div class="flex items-center">
+                    <img src="<?= getLogo($pick['team_id']); ?>"
+                         alt="<?= $pick['team_name']; ?> logo"
+                         height="35"/>
+                    <span><?= $pick['team_name']; ?></span>
+                  </div>
+                </td>
+                <td>
 									<span
-										class="badge <?= $pick['skin_select'] === 'wins' ? 'success' : 'danger'; ?>"
-									>
+                    class="badge <?= $pick['skin_select'] === 'wins' ? 'success' : 'danger'; ?>"
+                  >
 										<?= $pick['skin_select']; ?>
 									</span>
-								</td>
-								<td><?= $pick['wins']; ?></td>
-								<td><?= $pick['losses']; ?></td>
-								<td><?= $pick['skins']; ?></td>
-							</tr>
-						<?php endforeach; ?>
-						<tr>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td><strong>Total</strong></td>
-							<td>
-								<code>
-									<strong><?= $total; ?></strong>
-								</code>
-							</td>
-						</tr>
-						</tbody>
-					</table>
-				</div>
-			</article>
-		<?php endforeach; ?>
-	</div>
+                </td>
+                <td><?= $pick['wins']; ?></td>
+                <td><?= $pick['losses']; ?></td>
+                <td><?= $pick['skins']; ?></td>
+              </tr>
+            <?php endforeach; ?>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td><strong>Total</strong></td>
+              <td>
+                <code>
+                  <strong><?= $total; ?></strong>
+                </code>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </article>
+    <?php endforeach; ?>
+  </div>
 </section>
