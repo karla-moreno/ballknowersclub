e1ca51ab8ff3f6afc63aeb26bd17b56e18e479c9
karl <moreno_karla@live.com>
2026-05-01 00:15:22 -0500

    return 405 on register.php when not a POST call

--- public/register.php
+++ public/register.php
@@ -40,6 +40,11 @@
         }
       }
     }
+  } else {
+    http_response_code(405);
+    header('Content-Type: application/json');
+    echo json_encode(['success' => false, 'message' => 'Method not allowed.']);
+    exit;
   }
   $title = 'Register';
   $teams = (new NbaApiService)::allTeams();
